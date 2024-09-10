d4f5a3e5743cad8597ed31cad0c626ab2d32ccda
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2024-09-09 23:17:03 -0300

    update

--- basic-go-template/template/main.go
+++ basic-go-template/template/main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>It Works!!! Golang...</h1>"))
+		w.Write([]byte("<h1>It Works!!! AWS Community Day Argentina 2024...</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 
